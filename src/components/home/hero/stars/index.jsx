fe2c1baf154d3b915d1254276d3f32244ca57b0c
Nandini <nandiniverma202021@gmail.com>
2024-07-26 13:12:46 +0530

    refactor: client-testimonial

--- src/components/home/hero/stars/index.jsx
+++ src/components/home/hero/stars/index.jsx
@@ -1,8 +1,8 @@
 const stars = () => {
 
     {
-        let stars = [];
-        for (let i = 0; i < 5; i++) {
+        let stars = []
+        for ( let i = 0; i < 5; i++ ) {
             stars.push( 
                     <img 
                         alt="star_icon" 
